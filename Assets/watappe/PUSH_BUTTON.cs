4599da2f79148343eba6d7954c372214dc6834ea
watanabe <watanabe>
2016-10-21 17:00:18 +0900

    みすりました

---
+++ Assets/watappe/PUSH_BUTTON.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PUSH_BUTTON : MonoBehaviour {
+
+    enum TITELMENBER
+    {
+        title,
+        menu,
+
+    }
+
+    public GameObject[] m_tileMenber;
+
+    private TITELMENBER m_state;
+
+	// Use this for initialization
+	void Start () {
+        m_state = TITELMENBER.title;
+        m_tileMenber[(int)TITELMENBER.menu].SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(m_state)
+        {
+            case TITELMENBER.title:
+                if (!Input.GetMouseButtonDown(0)) break; ;
+                m_tileMenber[(int)m_state].SetActive(false);
+                m_tileMenber[(int)TITELMENBER.menu].SetActive(true);
+                m_state = TITELMENBER.menu;
+                break;
+            case TITELMENBER.menu:
+                break;
+        }
+	}
+}
---
+++ Assets/watappe/selection.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class selection : MonoBehaviour {
+
+    float move = -40;
+
+    float flag = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.DownArrow) && flag < 2)
+        {
+            transform.position = new Vector3(transform.position.x,
+                                         transform.position.y + move, transform.position.z);
+
+            flag += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && flag > 0)
+        {
+            transform.position = new Vector3(transform.position.x,
+                                         transform.position.y - move, transform.position.z);
+
+            flag -= 1;
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if(flag == 0)
+            {
+                SceneManager.LoadScene(0);
+            }
+            if (flag == 1)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+
+
+    }
+    
+}
