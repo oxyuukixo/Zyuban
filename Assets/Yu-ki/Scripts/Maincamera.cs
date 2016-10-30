54807be31496c5e1600c0f0d19fd6a8e63d29e5e
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-10-31 06:12:00 +0900

    no message

---
+++ Assets/Yu-ki/Scripts/Maincamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maincamera : MonoBehaviour
+{
+
+    public float MouseSpeedX = 10.0f;
+    public float MouseSpeedY = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(Input.GetAxis("Mouse Y") * MouseSpeedY, (Input.GetAxis("Mouse X") * MouseSpeedX), 0);
+
+        Quaternion qua = Quaternion.Euler(new Vector3(Input.GetAxis("Mouse Y") * MouseSpeedY, Input.GetAxis("Mouse X") * MouseSpeedX,0));
+    }
+}
