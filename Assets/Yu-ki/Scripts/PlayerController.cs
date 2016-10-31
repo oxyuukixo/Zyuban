a9715408d0ededbd2b58ddc8632914cbffb861c1
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-10-31 11:55:51 +0900

    アイテムを見つけて移動できるようにしました。

--- Assets/Yu-ki/Scripts/PlayerController.cs
+++ Assets/Yu-ki/Scripts/PlayerController.cs
@@ -1,14 +1,169 @@
-Ôªøusing UnityEngine;
-using System.Collections;
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using Random = UnityEngine.Random;
+using UnityStandardAssets.Characters.FirstPerson;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Camera m_Camera;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+
+    public bool clampVerticalRotation = true;
+
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
+
+    public bool smooth;
+    public float smoothTime = 5f;
+
+    public bool lockCursor = true;
+
+    private Quaternion m_CharacterTargetRot;
+    private Quaternion m_CameraTargetRot;
+    private bool m_cursorIsLocked = true;
+
+    public Transform target;
+
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    private void Start()
+    { 
+        m_Camera = Camera.main;
+        m_CharacterTargetRot = transform.localRotation;
+        m_CameraTargetRot = m_Camera.transform.localRotation;
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        LookRotation(transform, m_Camera.transform);
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            target = GameObject.Find("DefaultPosision").transform;
+        } 
+
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            ItemRayCheck();
+        }
+
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void ItemRayCheck()
+    {
+        Ray ray = new Ray(m_Camera.transform.position, m_Camera.transform.forward);
+
+        RaycastHit hit;
+        // RayÇ™è’ìÀÇµÇΩÇ©Ç«Ç§Ç©
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "KeyItem")
+            {
+                target = hit.collider.gameObject.transform;
+            }
+        }
 
     }
+
+    private void FixedUpdate()
+    {
+        UpdateCursorLock();
+    }
+
+    public void LookRotation(Transform character, Transform camera)
+    {
+        float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+        float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+
+        m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+        m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        if (clampVerticalRotation)
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+        if (smooth)
+        {
+            character.localRotation = Quaternion.Slerp(character.localRotation, m_CharacterTargetRot,
+                smoothTime * Time.deltaTime);
+            camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
+                smoothTime * Time.deltaTime);
+        }
+        else
+        {
+            character.localRotation = m_CharacterTargetRot;
+            camera.localRotation = m_CameraTargetRot;
+        }
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
 }
+
